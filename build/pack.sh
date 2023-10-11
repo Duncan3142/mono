b7e2c756352d25037b5b3cbd24b9ffbc54b5348c
Duncan Giles <duncan3142@gmail.com>
2023-10-11 21:33:45 +0100

    setup pnpm action

--- build/pack.sh
+++ build/pack.sh
@@ -4,5 +4,5 @@ set -e
 pnpm run turbo:build
 for p in $(pnpm -r list --depth=-1 --json | jq 'del(.[0]) | .[].path' -r);
 do
-  (cd $p; mkdir -p .package; npm pack --pack-destination .package);
+  (cd $p; mkdir -p .package; pnpm pack --pack-destination .package);
 done;
