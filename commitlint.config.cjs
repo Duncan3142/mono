4a08e7103a6263e947eccfcee6fb514159ef20cf
Duncan Giles <duncan3142@gmail.com>
2023-10-17 20:32:29 +0100

    build(@repo,plop-pkg): add commitlint

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,1 +1,17 @@
-module.exports = { extends: ["@commitlint/config-conventional"] }
+const { readdir } = require("node:fs/promises")
+
+const getScopes = () => readdir(`${__dirname}/pkgs`).then((scopes) => ["@repo", ...scopes])
+
+module.exports = {
+	extends: ["@commitlint/config-conventional"],
+	rules: {
+		"scope-empty": [2, "never"],
+		"scope-enum": () => Promise.all([2, "always", getScopes()]),
+	},
+	prompt: {
+		settings: {
+			enableMultipleScopes: true,
+			scopeEnumSeparator: ",",
+		},
+	},
+}
