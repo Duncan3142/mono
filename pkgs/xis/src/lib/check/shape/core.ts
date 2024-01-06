effb699f52a194eaad90ea7b0e9603fa99b7e3b6
Duncan Giles <duncan3142@gmail.com>
2024-01-06 09:27:28 +0000

    style(@repo): format

--- pkgs/xis/src/lib/check/shape/core.ts
+++ pkgs/xis/src/lib/check/shape/core.ts
@@ -82,8 +82,8 @@ export type BaseProp = Prop<BaseKey, XisBase>
 export type UnionToIntersection<T> = [T] extends [never]
 	? undefined
 	: (T extends any ? (x: T) => void : never) extends (x: infer R) => void
-	  ? R
-	  : never
+		? R
+		: never
 
 export interface MissingPropertyIssue extends XisIssue<"MISSING_PROPERTY"> {}
 
@@ -144,11 +144,11 @@ export type SchemaShape<S extends [...Array<BaseProp>]> = SchemaCore<S> extends 
 	? RecordIntersection<
 			Mutable<Pick<SC, MutableKeys<S>[number]>>,
 			Readonly<Pick<SC, ReadonlyKeys<S>[number]>>
-	  > extends infer RM
+		> extends infer RM
 		? RecordIntersection<
 				Required<Pick<RM, RequiredKeys<S>[number]>>,
 				Partial<Pick<RM, OptionalKeys<S>[number]>>
-		  >
+			>
 		: never
 	: never
 
@@ -187,7 +187,7 @@ export type StripShapeIn<S extends [...Array<BaseProp>]> = SchemaShape<S> extend
 			[P in keyof SS]: Exclude<SS[P], undefined> extends infer C extends XisBase
 				? ExIn<C>
 				: never
-	  }
+		}
 	: never
 
 export type StripShapeOut<S extends [...Array<BaseProp>]> = SchemaShape<S> extends infer SS
@@ -195,7 +195,7 @@ export type StripShapeOut<S extends [...Array<BaseProp>]> = SchemaShape<S> exten
 			[P in keyof SS]: Exclude<SS[P], undefined> extends infer C extends XisBase
 				? ExOut<C>
 				: never
-	  }
+		}
 	: never
 
 export type ShapeIn<S extends [...Array<BaseProp>], CM extends ShapeCheckMode> = [CM] extends [
