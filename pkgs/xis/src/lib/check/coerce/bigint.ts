7c5b99db6445aa0cc742eff7832b0da8fee74bfa
Duncan Giles <duncan3142@gmail.com>
2024-01-24 22:45:57 +0000

    refactor: coerce [no ci]

--- pkgs/xis/src/lib/check/coerce/bigint.ts
+++ pkgs/xis/src/lib/check/coerce/bigint.ts
@@ -1,38 +1,63 @@
-import type { XisArgObjBase } from "#core/context.js"
-import { XisSync, type ExecResultSync, type ParseResultSync } from "#core/sync.js"
+import { XisSync, type ExecResultSync } from "#core/sync.js"
 import { trueTypeOf } from "#util/base-type.js"
-import { Either, Right } from "purify-ts/Either"
-import { coerceErr, type CoerceIssue } from "./core.js"
+import { Either, Left, Right } from "purify-ts/Either"
+import {
+	coerceIssue,
+	XIS_COERCE,
+	type CoerceIssue,
+	type XisCoerceArgs,
+	type XisCoerceMessages,
+} from "./core.js"
+import type { XisExecArgs } from "#core/args.js"
 
 export type BigIntInput = bigint | number | string | boolean
 
-export class XisCoerceBigInt extends XisSync<
-	BigIntInput,
-	CoerceIssue<"bigint">,
-	CoerceIssue<"bigint">,
-	bigint
-> {
-	parse(
-		value: unknown,
-		ctx: XisArgObjBase
-	): ParseResultSync<CoerceIssue<"bigint">, CoerceIssue<"bigint">, bigint> {
+function isBigIntInput(valueType: string, value: unknown): value is BigIntInput {
+	switch (valueType) {
+		case "number":
+		case "string":
+		case "boolean":
+			return true
+		default:
+			return false
+	}
+}
+
+export class XisCoerceBigInt extends XisSync<unknown, CoerceIssue, bigint> {
+	#messages: XisCoerceMessages
+	constructor(args: XisCoerceArgs) {
+		super()
+		this.#messages = args.messages ?? {
+			XIS_COERCE,
+		}
+	}
+	exec(args: XisExecArgs): ExecResultSync<CoerceIssue, bigint> {
+		const { value, locale, path } = args
 		const valueType = trueTypeOf(value)
 		if (valueType === "bigint") {
 			return Right(value as bigint)
 		}
-		if (valueType === "number" || valueType === "string" || valueType === "boolean") {
-			return this.exec(value as BigIntInput, ctx)
-		}
-
-		return coerceErr("bigint", value, valueType, ctx)
-	}
-
-	exec(value: BigIntInput, ctx: XisArgObjBase): ExecResultSync<CoerceIssue<"bigint">, bigint> {
-		return Either.encase(() => BigInt(value)).chainLeft((_) => {
-			const valueType = trueTypeOf(value)
-			return coerceErr("bigint", value, valueType, ctx)
+		const res = isBigIntInput(valueType, value) ? Either.encase(() => BigInt(value)) : Left(NaN)
+		return res.chainLeft((_) => {
+			const message = this.#messages.XIS_COERCE({
+				value,
+				path,
+				locale,
+				ctx: null,
+				props: {
+					desired: "bigint",
+					type: valueType,
+				},
+			})
+			return coerceIssue({
+				desired: "bigint",
+				received: value,
+				type: valueType,
+				message,
+				path,
+			})
 		})
 	}
 }
 
-export const bigint: XisCoerceBigInt = new XisCoerceBigInt()
+export const bigint = (args: XisCoerceArgs) => new XisCoerceBigInt(args)
--- pkgs/xis/src/lib/check/coerce/boolean.ts
+++ pkgs/xis/src/lib/check/coerce/boolean.ts
@@ -1,13 +1,10 @@
-import { XisSync, type ExecResultSync, type ParseResultSync } from "#core/sync.js"
+import type { XisExecArgs } from "#core/args.js"
+import { XisSync, type ExecResultSync } from "#core/sync.js"
 import { Right } from "purify-ts/Either"
 
-export class XisCoerceBoolean extends XisSync<unknown, never, never, boolean> {
-	parse(value: unknown): ParseResultSync<never, never, boolean> {
-		return this.exec(value)
-	}
-
-	exec(value: unknown): ExecResultSync<never, boolean> {
-		return Right(Boolean(value))
+export class XisCoerceBoolean extends XisSync<unknown, never, boolean> {
+	exec(args: XisExecArgs): ExecResultSync<never, boolean> {
+		return Right(Boolean(args.value))
 	}
 }
 
