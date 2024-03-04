40b201af43ea16d5d5c59b0b9b00012b8df3499d
Duncan Giles <duncan3142@gmail.com>
2024-03-04 21:18:59 +0000

    test no op

--- actions/changesets/publish.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-
-echo Running publish...
-pnpm exec changeset publish
-git push --follow-tags --no-verify
--- actions/changesets/version.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-
-pnpm exec changeset version
-pnpm install --no-frozen-lockfile
-git add pnpm-lock.yaml
