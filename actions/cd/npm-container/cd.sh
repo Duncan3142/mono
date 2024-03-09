7f80b68896e56163ac84539aa9bea1efffc23b78
Duncan Giles <duncan3142@gmail.com>
2024-03-09 22:25:00 +0000

    safe directory

--- actions/cd/npm-container/cd.sh
+++ actions/cd/npm-container/cd.sh
@@ -7,6 +7,7 @@ set -e
 
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git config --global --add safe.directory "${GITHUB_WORKSPACE}"
 
 gh auth setup-git
 
