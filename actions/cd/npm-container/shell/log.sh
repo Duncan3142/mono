177bf4407461efe03c9913c3b40a9f75c3684905
Duncan Giles <duncan3142@gmail.com>
2024-03-09 22:49:54 +0000

    log colors

--- actions/cd/npm-container/shell/log.sh
+++ actions/cd/npm-container/shell/log.sh
@@ -8,10 +8,42 @@ export LOG_LEVEL_ERROR=4
 
 export LOG_LEVEL=${LOG_LEVEL:-$LOG_LEVEL_INFO}
 
-function log_info() {
+# High Intensity
+ICyan='\033[0;96m'        # Cyan
+IBlue='\033[0;94m'        # Blue
+IGreen='\033[0;92m'       # Green
+IYellow='\033[0;93m'      # Yellow
+IRed='\033[0;91m'         # Red
+INC='\033[0m'             # No Color
+
+function log_trace() {
 	if [[ $LOG_LEVEL -le $LOG_LEVEL_INFO ]]; then
-		echo "$*"
+		echo -e "${ICyan}$*${INC}"
 	fi
 }
 
-export -f log_info
+function log_debug() {
+	if [[ $LOG_LEVEL -le $LOG_LEVEL_INFO ]]; then
+		echo -e "${IBlue}$*${INC}"
+	fi
+}
+
+function log_info() {
+	if [[ $LOG_LEVEL -le $LOG_LEVEL_INFO ]]; then
+		echo -e "${IGreen}$*${INC}"
+	fi
+}
+
+function log_warn() {
+	if [[ $LOG_LEVEL -le $LOG_LEVEL_INFO ]]; then
+		echo -e "${IYellow}$*${INC}"
+	fi
+}
+
+function log_error() {
+	if [[ $LOG_LEVEL -le $LOG_LEVEL_INFO ]]; then
+		echo -e "${IRed}$*${INC}"
+	fi
+}
+
+export -f log_trace log_debug log_info log_warn log_error
