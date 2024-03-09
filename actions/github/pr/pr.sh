1916aa6fd88b724fbfd9694d1c151aa7629c40c6
Duncan Giles <duncan3142@gmail.com>
2024-03-09 01:32:48 +0000

    debug pr fetch

--- actions/github/pr/pr.sh
+++ actions/github/pr/pr.sh
@@ -4,17 +4,17 @@ CHANGES=false
 
 git add .
 
-HEAD_REF=$(git rev-parse --abbrev-ref HEAD)
+HEAD_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 if git commit -m "Semver \"${PKG_NAME}\""; then
 	CHANGES=true
-	git push --force-with-lease "${REMOTE}" "${HEAD_REF}"
+	git push --force-with-lease "${REMOTE}" "${HEAD_BRANCH}"
 fi
 
 if ! URL=$(gh pr create --base "${BASE_BRANCH}" --title "SemVer \"${PKG_NAME}\"" --body "This is an auto generated PR to semantically version \"${PKG_NAME}\"" --label ci --label semver); then
-	URL=$(gh pr list --base "${BASE_BRANCH}" --head "${HEAD_REF}" --label ci --label semver --json url --jq '.[0].url')
+	URL=$(gh pr list --base "${BASE_BRANCH}" --head "${HEAD_BRANCH}" --label ci --label semver --json url --jq '.[0].url')
 	if [ -z "${URL}" ]; then
-		echo "Failed to find existing PR"
+		echo "Failed to find existing SemVer PR with base \"${BASE_BRANCH}\" and head \"${HEAD_BRANCH}\""
 		exit 1
 	fi
 fi
