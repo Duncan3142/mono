87bb16e78882dccba5e0be422eea2f361ea68f6c
Duncan Giles <duncan3142@gmail.com>
2024-03-10 16:32:39 +0000

    version script

--- actions/github/pr/pr.sh
+++ actions/github/pr/pr.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-CHANGES=false
+VERSION_UPDATED=false
 
 git add .
 
 HEAD_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 if git commit -m "Semver \"${PKG_NAME}\""; then
-	CHANGES=true
+	VERSION_UPDATED=true
 	git push --force-with-lease "${REMOTE}" "${HEAD_BRANCH}"
 fi
 
-if ! URL=$(gh pr create --base "${BASE_BRANCH}" --title "SemVer \"${PKG_NAME}\"" --body "This is an auto generated PR to semantically version \"${PKG_NAME}\"" --label ci --label semver); then
-	URL=$(gh pr list --base "${BASE_BRANCH}" --head "${HEAD_BRANCH}" --json url --jq '.[0].url')
-	if [ -z "${URL}" ]; then
+if ! PR_URL=$(gh pr create --base "${BASE_BRANCH}" --title "SemVer \"${PKG_NAME}\"" --body "This is an auto generated PR to semantically version \"${PKG_NAME}\"" --label ci --label semver); then
+	PR_URL=$(gh pr list --base "${BASE_BRANCH}" --head "${HEAD_BRANCH}" --json url --jq '.[0].url')
+	if [ -z "${PR_URL}" ]; then
 		echo "Failed to find existing SemVer PR with base \"${BASE_BRANCH}\" and head \"${HEAD_BRANCH}\""
 		exit 1
 	fi
@@ -21,4 +21,4 @@ fi
 
 # gh pr merge --auto --squash
 
-export CHANGES URL
+export VERSION_UPDATED PR_URL
