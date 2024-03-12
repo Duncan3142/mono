8ce5dd6b9160cf6179aed4d7f3dc1804495c0039
Duncan Giles <duncan3142@gmail.com>
2024-03-12 14:18:59 +0000

    wait for all with status

--- actions/debug/temp.sh
+++ actions/debug/temp.sh
@@ -1,10 +1,31 @@
 #!/usr/bin/env bash
 
+pids=()
+
 (
 	echo -e "WORK DIR: $(pwd)\n\n\n" &&
   sleep 4s &&
 	echo -e "\n\n\nMEOW WOOF"
-) &
-env &
-wait
+) &> /tmp/sleepy.txt &
+pids+=($!)
+(sleep 1s && exit 0) &
+pids+=($!)
+env &> /tmp/env.txt &
+pids+=($!)
+echo "${pids[*]}"
+status=0
+for i in "${!pids[@]}"
+do
+	if ! wait "${pids[i]}"; then
+		status=1
+	fi
+	unset "pids[$i]"
+done
+echo "SLEEPY.TXT"
+cat /tmp/sleepy.txt
+echo ""
+echo "ENV.TXT"
+cat /tmp/env.txt
+echo ""
 echo "ALL DONE"
+exit "${status}"
