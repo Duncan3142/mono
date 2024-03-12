dc9f8518f833598739f6bf8a13dea37552866894
Duncan Giles <duncan3142@gmail.com>
2024-03-12 02:39:11 +0000

    config

--- actions/debug/echo.sh
+++ actions/debug/echo.sh
@@ -2,4 +2,12 @@
 
 echo "MAIN MAIN MAIN MAIN MAIN MAIN"
 echo "PATH: ${PATH}"
+
+echo "GIT CONFIG"
 git config --list
+
+echo "GLOBAL GIT CONFIG"
+cat ~/.gitconfig
+
+echo "LOCAL GIT CONFIG"
+cat ./.gitconfig
