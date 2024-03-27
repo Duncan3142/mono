467553010f8d7fc717932012f50d9906ae3cd56c
Duncan Giles <duncan3142@gmail.com>
2024-03-27 17:47:41 +0000

    fix formatting

--- pkgs/prettier-config/.prettierrc.js
+++ pkgs/prettier-config/.prettierrc.js
@@ -1,7 +1,7 @@
 export default {
-	"semi": false,
-	"trailingComma": "es5",
-	"singleQuote": false,
-	"printWidth": 96,
-	"useTabs": true
+	semi: false,
+	trailingComma: "es5",
+	singleQuote: false,
+	printWidth: 96,
+	useTabs: true,
 }
