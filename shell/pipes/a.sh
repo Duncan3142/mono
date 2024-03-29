9ecd849354394e043db32c2912680fcac3219dee
Duncan Giles <duncan3142@gmail.com>
2024-03-29 20:31:51 +0000

    restore pipe

--- shell/pipes/a.sh
+++ shell/pipes/a.sh
@@ -7,9 +7,9 @@ exec {stdOutFD}>& 1
 
 echo Running a.sh
 
-# ( exec {outFD}>& 1 && ./b.sh "/dev/fd/$outFD" >& $stdOutFD; exit 1 ) |
-# ( exec {outFD}>& 1 && ./c.sh /dev/stdin "/dev/fd/$outFD" >& $stdOutFD ) |
-# ./d.sh /dev/stdin
+( exec {outFD}>& 1 && ./b.sh "/dev/fd/$outFD" >& $stdOutFD ) |
+( exec {outFD}>& 1 && ./c.sh /dev/stdin "/dev/fd/$outFD" >& $stdOutFD ) |
+./d.sh /dev/stdin
 
 # (
 # 	(./d.sh /dev/stdin) < \
@@ -17,8 +17,8 @@ echo Running a.sh
 # ) < \
 # <( exec {outFD}>& 1 && ./b.sh "/dev/fd/$outFD" >& $stdOutFD )
 
-(
-	( exec {outFD}>& 1 && ./b.sh "/dev/fd/$outFD" >& $stdOutFD ) > \
-	>( exec {outFD}>& 1 && ./c.sh /dev/stdin "/dev/fd/$outFD" >& $stdOutFD )
-) > \
->(./d.sh /dev/stdin)
+# (
+# 	( exec {outFD}>& 1 && ./b.sh "/dev/fd/$outFD" >& $stdOutFD ) > \
+# 	>( exec {outFD}>& 1 && ./c.sh /dev/stdin "/dev/fd/$outFD" >& $stdOutFD )
+# ) > \
+# >(./d.sh /dev/stdin)
