fd8a1e1c1dd06e450770e51c97eb17a621e44e74
Duncan Giles <duncan3142@gmail.com>
2024-03-29 20:01:35 +0000

    substitution

--- shell/pipes/a.sh
+++ shell/pipes/a.sh
@@ -7,6 +7,18 @@ exec {stdOutFD}>& 1
 
 echo Running a.sh
 
-( exec {outFD}>& 1 && ./b.sh "/dev/fd/$outFD" >& $stdOutFD ) |
-( exec {outFD}>& 1 && ./c.sh /dev/stdin "/dev/fd/$outFD" >& $stdOutFD ) |
-./d.sh /dev/stdin
+# ( exec {outFD}>& 1 && ./b.sh "/dev/fd/$outFD" >& $stdOutFD; exit 1 ) |
+# ( exec {outFD}>& 1 && ./c.sh /dev/stdin "/dev/fd/$outFD" >& $stdOutFD ) |
+# ./d.sh /dev/stdin
+
+# (
+# 	(./d.sh /dev/stdin) < \
+# 	<( exec {outFD}>& 1 && ./c.sh /dev/stdin "/dev/fd/$outFD" >& $stdOutFD )
+# ) < \
+# <( exec {outFD}>& 1 && ./b.sh "/dev/fd/$outFD" >& $stdOutFD )
+
+(
+	( exec {outFD}>& 1 && ./b.sh "/dev/fd/$outFD" >& $stdOutFD ) > \
+	>( exec {outFD}>& 1 && ./c.sh /dev/stdin "/dev/fd/$outFD" >& $stdOutFD )
+) > \
+>(./d.sh /dev/stdin)
