71adc5d1bb4e625a0e8023c45ef487fc8531b622
Duncan Giles <duncan3142@gmail.com>
2024-07-27 19:29:37 +0100

    tidy changes cli

--- tools/changes-cli/main.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-import chalk from 'chalk'
-
-const {redBright, blueBright, greenBright} = chalk
-
-console.log(`${redBright('Hello')} ${blueBright('Nodejs')}${greenBright('!')}`)
--- tools/changes-cli/src/test.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-import {parseEnv as parseLogEnv, Logger} from './lib/log.js'
-import {configuration} from './lib/config.js'
-
-const {env,raw} = configuration({parseLogEnv})
-const logger = new Logger(env.log)
-
-logger.debug('Hello, Nodejs!', raw)
