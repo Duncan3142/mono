137f19a70dc3e123bf8392b9c8d2891b63968089
Duncan Giles <duncan3142@gmail.com>
2025-01-16 19:56:40 +0000

    config

--- apps/svelte-squared/eslint.config.js
+++ apps/svelte-squared/eslint.config.js
@@ -1,9 +1,42 @@
-import { configsArrFactory, parsers } from "@duncan3142/eslint-config"
+// @ts-check
+
+import {
+	configsArrFactory,
+	ElementMode,
+	parsers,
+	plugins,
+	configBuilder,
+} from "@duncan3142/eslint-config"
 import svelte from "eslint-plugin-svelte"
+import svelteParser from "svelte-eslint-parser"
 import globals from "globals"
 
-export default ts.config(
-	...configsArrFactory(),
+export default configBuilder(
+	...configsArrFactory({
+		boundaries: {
+			settings: {
+				elements: [
+					{ type: "cnfg", pattern: [".*", "*"], mode: ElementMode.Full },
+					{ type: "src", pattern: ["src"], mode: ElementMode.Folder },
+				],
+			},
+			rules: {
+				elements: [
+					{
+						from: ["src"],
+						allow: ["src"],
+					},
+				],
+				entry: [
+					{
+						target: ["src"],
+						allow: ["index.ts"],
+					},
+				],
+				external: [{ from: ["*"], allow: ["node:*"] }],
+			},
+		},
+	}),
 	...svelte.configs["flat/recommended"],
 	...svelte.configs["flat/prettier"],
 	{
@@ -16,10 +49,17 @@ export default ts.config(
 	},
 	{
 		files: ["**/*.svelte"],
+		plugins,
 
 		languageOptions: {
+			parser: svelteParser,
 			parserOptions: {
 				parser: parsers.typescript,
+				parserOptions: {
+					// project: './path/to/your/tsconfig.json',
+					projectService: true,
+					extraFileExtensions: [".svelte"],
+				},
 			},
 		},
 	}
