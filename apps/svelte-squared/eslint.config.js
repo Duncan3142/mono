e0876e379eefce1e3e23aa52fc39cf7aebab2d28
Duncan Giles <duncan3142@gmail.com>
2025-01-17 23:07:14 +0000

    functional eslint config

--- apps/svelte-squared/eslint.config.js
+++ apps/svelte-squared/eslint.config.js
@@ -1,60 +1,79 @@
 // @ts-check
 
-import {
-	configsArrFactory,
-	ElementMode,
-	parsers,
-	plugins,
-	configBuilder,
-} from "@duncan3142/eslint-config"
+import boundaries, { devDependencies, ElementMode } from "@duncan3142/eslint-config/boundaries"
+import base from "@duncan3142/eslint-config/base"
+import jsdoc from "@duncan3142/eslint-config/jsdoc"
+import secrets from "@duncan3142/eslint-config/secrets"
+import promise from "@duncan3142/eslint-config/promise"
+import typescript, { untyped, parser } from "@duncan3142/eslint-config/typescript"
+import { ignored } from "@duncan3142/eslint-config/ignored"
+import prettier from "@duncan3142/eslint-config/prettier"
+import comments from "@duncan3142/eslint-config/comments"
+import core, { compose, filePatterns, jsExtensions } from "@duncan3142/eslint-config/core"
+
 import svelte from "eslint-plugin-svelte"
 import svelteParser from "svelte-eslint-parser"
 import globals from "globals"
 
-export default configBuilder(
-	...configsArrFactory({
-		boundaries: {
-			settings: {
-				elements: [
-					{ type: "cnfg", pattern: [".*", "*"], mode: ElementMode.Full },
-					{ type: "src", pattern: ["src"], mode: ElementMode.Folder },
-				],
+const boundaryOptions = {
+	settings: {
+		elements: [
+			{ type: "cnfg", pattern: [".*", "*"], mode: ElementMode.Full },
+			{ type: "src", pattern: ["src"], mode: ElementMode.Folder },
+		],
+	},
+	rules: {
+		elements: [
+			{
+				from: ["src"],
+				allow: ["src"],
 			},
-			rules: {
-				elements: [
-					{
-						from: ["src"],
-						allow: ["src"],
-					},
-				],
-				entry: [
-					{
-						target: ["src"],
-						allow: ["index.ts"],
-					},
-				],
-				external: [{ from: ["*"], allow: ["node:*"] }],
+		],
+		entry: [
+			{
+				target: ["src"],
+				allow: ["index.ts"],
 			},
+		],
+		external: [{ from: ["*"], allow: ["node:*"] }],
+	},
+	tsConfigs: ["tsconfig.json", ".svelte-kit/tsconfig.json"],
+}
+
+export default compose(
+	core,
+	ignored(),
+	base,
+	comments,
+	typescript({
+		parserOptions: {
+			project: ["tsconfig.json", ".svelte-kit/tsconfig.json"],
+			extraFileExtensions: [".svelte"],
 		},
-		tsConfigs: ["tsconfig.json", ".svelte-kit/tsconfig.json"],
 	}),
-	...svelte.configs["flat/recommended"],
-	...svelte.configs["flat/prettier"],
+	untyped({ files: filePatterns(jsExtensions, "svelte") }),
+	// boundaries(boundaryOptions),
+	// devDependencies,
+	promise,
+	jsdoc,
+	secrets,
+	svelte.configs["flat/recommended"],
 	{
-		// files: ["**/*.svelte"],
-		plugins,
+		files: ["**/*.svelte"],
 		languageOptions: {
 			ecmaVersion: 2024,
 			sourceType: "module",
 			globals: { ...globals.node, ...globals.browser },
 			parser: svelteParser,
 			parserOptions: {
-				parser: parsers.typescript,
+				parser,
 				parserOptions: {
 					project: ["tsconfig.json", ".svelte-kit/tsconfig.json"],
 					extraFileExtensions: [".svelte"],
 				},
 			},
 		},
-	}
+	},
+	prettier,
+	svelte.configs["flat/prettier"]
 )
