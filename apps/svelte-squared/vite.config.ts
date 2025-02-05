620b7e2361ea26545b64cbacab26fbe4d26d5e69
Duncan Giles <duncan3142@gmail.com>
2025-02-05 15:54:53 +0000

    format

--- apps/svelte-squared/vite.config.ts
+++ apps/svelte-squared/vite.config.ts
@@ -1,7 +1,7 @@
 import { paraglide as paraglideFactory } from "@inlang/paraglide-sveltekit/vite"
 import { defineConfig } from "vitest/config"
 import { sveltekit } from "@sveltejs/kit/vite"
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite"
 import type { Plugin } from "vite"
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment -- Package uses 'any' type
