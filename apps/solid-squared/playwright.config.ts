67d709d7f0802e76a2d593c8d32410388029fa49
Duncan Giles <duncan3142@gmail.com>
2025-03-22 22:54:31 +0000

    packages

--- apps/solid-squared/playwright.config.ts
+++ apps/solid-squared/playwright.config.ts
@@ -5,7 +5,6 @@ export default defineConfig({
 		command: "npm run build && npm run preview",
 		port: 4173,
 	},
-
 	testDir: "e2e",
 	outputDir: ".test-results",
 })
--- apps/solid-squared/vite.config.ts
+++ apps/solid-squared/vite.config.ts
@@ -1,19 +1,20 @@
-import { paraglide as paraglideFactory } from "@inlang/paraglide-sveltekit/vite"
-import { defineConfig } from "vitest/config"
-import { sveltekit } from "@sveltejs/kit/vite"
-import type { Plugin } from "vite"
-import UnoCSS from 'unocss/vite';
-
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment -- Package uses 'any' type
-const paraglide: Plugin = paraglideFactory({
-	project: "./project.inlang",
-	outdir: "./src/lib/paraglide",
-})
+import { defineConfig } from "vite"
+import unoCSS from "unocss/vite"
+import solid from "vite-plugin-solid"
+import { paraglideVitePlugin as paraglide } from "@inlang/paraglide-js"
 
 export default defineConfig({
-	plugins: [UnoCSS(), sveltekit(), paraglide],
+	plugins: [
+		unoCSS(),
+		solid(),
+		paraglide({
+			project: "./project.inlang",
+			outdir: "./src/lib/paraglide",
+		}),
+	],
 	test: {
 		environment: "jsdom",
 		include: ["src/**/*.{test,spec}.{js,ts}"],
+		setupFiles: ["node_modules/@testing-library/jest-dom/vitest"],
 	},
 })
