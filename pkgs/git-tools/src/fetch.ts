3092624e7dbd1cbc9bd8cf7dc8f235d6a6b794df
Duncan Giles <duncan3142@gmail.com>
2025-04-06 16:13:14 +0100

    fetch script

---
+++ pkgs/git-tools/src/fetch.ts
@@ -0,0 +1,62 @@
+import type { ExecaMethod } from "execa"
+
+/**
+ * Fetches refs the remote repository.
+ * @param $ - The execa method to run the command
+ * @returns - A promise that resolves when the command is complete
+ */
+const fetch = async ($: ExecaMethod): Promise<void> => {
+	await $`git fetch --help`
+	return
+}
+
+const bash = /* bash */ `
+	set -ueC
+	set -o pipefail
+
+	branches=()
+	tags=()
+	while (( "$#" )); do
+		case $1 in
+			-t | --tag)
+				tags=("$2")
+				shift 2
+				;;
+			-d | --depth)
+				GIT_FETCH_DEPTH=$2
+				shift 2
+				;;
+			*)
+				branches+=("$1")
+				shift
+				;;
+		esac
+	done
+
+	GIT_FETCH_DEPTH=\${GIT_FETCH_DEPTH:-1}
+
+	refSpecs=()
+	for branch in "\${branches[@]}"; do
+		refSpecs+=("refs/heads/\${branch}:refs/remotes/\${GIT_REMOTE}/\${branch}")
+	done
+	for tag in "\${tags[@]}"; do
+		refSpecs+=("refs/tags/\${tag}:refs/tags/\${tag}")
+	done
+
+	if timber -l debug; then
+		timber debug "Refs pre fetch"
+		git-refs
+	fi
+
+	timber info "Fetching ref specs \${refSpecs[*]}"
+
+	git fetch "\${GIT_REMOTE}" --depth="\${GIT_FETCH_DEPTH}" "\${refSpecs[@]}"
+
+	if timber -l debug; then
+		timber debug "Refs post fetch"
+		git-refs
+	fi
+`
+
+export { bash }
+export default fetch
