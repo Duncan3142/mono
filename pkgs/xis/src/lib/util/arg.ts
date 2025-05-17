cde779772d5292b3596224526d5c034a02fe3937
Duncan Giles <duncan3142@gmail.com>
2025-05-17 22:28:26 +0100

    delete xis

--- pkgs/xis/src/lib/util/arg.ts
+++
@@ -1,5 +0,0 @@
-import type { TruePropertyKey } from "./base-type.js"
-
-export type ObjArgBase = Record<TruePropertyKey, unknown>
-
-export type BuildObjArg<Current extends ObjArgBase, Next extends ObjArgBase> = Current & Next
--- pkgs/xis/src/lib/util/never-guard.ts
+++
@@ -1,5 +0,0 @@
-/* c8 ignore start */
-export const neverGuard = (...values: Array<never>): never => {
-	throw new Error(`Unexpected values: ${String(values)}`)
-}
-/* c8 ignore stop */
