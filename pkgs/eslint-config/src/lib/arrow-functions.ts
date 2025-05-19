eac6d8f936f4ff23865288d8bf2ef3a32c97836b
Duncan Giles <duncan3142@gmail.com>
2025-05-19 07:40:20 +0100

    lint rules

--- pkgs/eslint-config/src/lib/arrow-functions.ts
+++ pkgs/eslint-config/src/lib/arrow-functions.ts
@@ -1,1 +1,16 @@
-// eslint-disable-next-line unicorn/no-empty-file -- ::TODO::
+import arrow from "eslint-plugin-prefer-arrow-functions"
+import { filePatterns, jstsExtensions, type MutableConfig } from "./core.ts"
+
+// eslint-disable-next-line functional/prefer-immutable-types -- Package type
+const { rules } = arrow
+
+const custom: MutableConfig = {
+	name: "@duncan3142/eslint-config/prefer-arrow-functions",
+	files: filePatterns(...jstsExtensions),
+	plugins: { "prefer-arrow-functions": { rules } },
+	rules: {
+		"prefer-arrow-functions/prefer-arrow-functions": ["error"],
+	},
+}
+
+export default custom
--- pkgs/eslint-config/src/lib/remeda.ts
+++ pkgs/eslint-config/src/lib/remeda.ts
@@ -1,1 +1,12 @@
-// eslint-disable-next-line unicorn/no-empty-file -- ::TODO::
+import remeda from "eslint-plugin-remeda"
+import { compose, filePatterns, jstsExtensions, type MutableConfig } from "./core.ts"
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-type-assertion -- eslint-plugin-remeda types are not correct
+const config: MutableConfig = compose({
+	name: "@duncan3142/eslint-config/remeda",
+	files: filePatterns(...jstsExtensions),
+	// @ts-expect-error -- eslint-plugin-remeda types are not correct
+	extends: [remeda.configs.recommended],
+}) as unknown as MutableConfig
+
+export default config
