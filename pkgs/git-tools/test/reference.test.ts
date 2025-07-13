960be1bb934646bbd8e1717687f1782cefa18f67
Duncan Giles <duncan3142@gmail.com>
2025-07-13 18:10:04 +0100

    test log output

--- pkgs/git-tools/test/reference.test.ts
+++ pkgs/git-tools/test/reference.test.ts
@@ -1,4 +1,4 @@
-import { expect, describe, it } from "@effect/vitest"
+import { expect, describe, it, vi } from "@effect/vitest"
 import {
 	exit as effectExit,
 	gen as effectGen,
@@ -6,9 +6,11 @@ import {
 	void as effectVoid,
 } from "effect/Effect"
 import { provide as layerProvide } from "effect/Layer"
+import { make as loggerMake, replace as loggerReplace, defaultLogger } from "effect/Logger"
 import { pipe } from "effect/Function"
 import { NodeContext } from "@effect/platform-node"
-import { pretty as loggerPretty } from "effect/Logger"
+import { type Console, withConsole } from "effect/Console"
+import { mockDeep } from "vitest-mock-extended"
 import ReferenceLayer from "#reference/layer"
 import Reference from "#reference/service"
 import PrintLayer from "#reference/git/print.layer"
@@ -18,6 +20,15 @@ const MainLayer = pipe(
 	layerProvide(PrintLayer),
 	layerProvide(NodeContext.layer)
 )
+
+const logHandler = vi.fn()
+
+const loggerLayer = loggerReplace(defaultLogger, loggerMake(logHandler))
+
+const MockConsole = mockDeep<Console>()
+
+// MockConsole.log.mockImplementation(() => effectVoid)
+// MockConsole.unsafe.log.mockImplementation(() => effectVoid)
 
 describe("Reference Layer", () => {
 	it.effect("prints references", () =>
@@ -31,11 +42,18 @@ describe("Reference Layer", () => {
 						message: "Testing print references",
 					})
 				)
-
 				expect(result).toStrictEqual(effectVoid)
+
+				expect(logHandler).toHaveBeenCalledWith(
+					expect.objectContaining({
+						message: ["Testing print references"],
+						logLevel: expect.objectContaining({ label: "INFO" }),
+					})
+				)
 			}),
 			effectProvide(MainLayer),
-			effectProvide(loggerPretty)
+			effectProvide(loggerLayer),
+			withConsole(MockConsole)
 		)
 	)
 })
