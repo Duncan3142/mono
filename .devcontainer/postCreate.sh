0e29b22000481b6a24dca1e287c75af92c242444
Duncan Giles <duncan3142@gmail.com>
2025-08-02 15:16:26 +0000

    devcontainer docker

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -4,5 +4,3 @@ set -ueC
 set -o pipefail
 
 exit 0
-# echo "Add group write permission to /workspace"
-# chmod g+w /workspace
--- .devcontainer/postStart.sh
+++ .devcontainer/postStart.sh
@@ -4,5 +4,3 @@ set -ueC
 set -o pipefail
 
 exit 0
-# echo "Sets the 'setgid' bit, to ensure new files inherit group ownership."
-# chmod g+s /workspace
