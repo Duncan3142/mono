7af62851c888c1e7d572015e5b690ec730dcfeed
Duncan Giles <duncan3142@gmail.com>
2025-08-10 19:08:22 +0000

    tag executor

---
+++ pkgs/git-tools/src/lib/git/executor/tag.layer.ts
@@ -0,0 +1,42 @@
+import { CommandExecutor } from "@effect/platform"
+import { Layer, pipe, Effect, Match, Console } from "effect"
+import * as Base from "./base.ts"
+import { TagExecutor } from "#duncan3142/git-tools/executor"
+import { type GitCommandError, TagMode } from "#duncan3142/git-tools/domain"
+
+const Live: Layer.Layer<TagExecutor.TagExecutor, never, CommandExecutor.CommandExecutor> =
+	Layer.effect(
+		TagExecutor.TagExecutor,
+		Effect.gen(function* () {
+			const executor = yield* CommandExecutor.CommandExecutor
+
+			return ({
+				mode,
+				directory,
+				timeout,
+			}: TagExecutor.Arguments): Effect.Effect<
+				void,
+				GitCommandError.GitCommandFailed | GitCommandError.GitCommandTimeout
+			> => {
+				const [subArgs, noPager] = TagMode.$match(mode, {
+					Create: ({ name }) => [[name], false] as const,
+					Print: () => [[], true] as const,
+				})
+				return pipe(
+					Base.make({
+						directory,
+						noPager,
+						subCommand: "tag",
+						subArgs,
+						timeout,
+						errorMatcher: Match.value,
+					}),
+					Effect.flatMap(Console.log),
+					Effect.scoped,
+					Effect.provideService(CommandExecutor.CommandExecutor, executor)
+				)
+			}
+		})
+	)
+
+export { Live }
