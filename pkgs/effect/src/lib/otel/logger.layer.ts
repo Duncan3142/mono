aaebef6d058a7643dbf695e57987f292bceb5027
Duncan Giles <duncan3142@gmail.com>
2025-09-27 16:45:33 +0000

    effect initial release

--- pkgs/effect/src/lib/otel/logger.layer.ts
+++ pkgs/effect/src/lib/otel/logger.layer.ts
@@ -13,31 +13,40 @@ import {
 import { Logger as OtelLogger } from "@effect/opentelemetry"
 import type { SdkLogRecord } from "@opentelemetry/sdk-logs"
 
+import { ExhaustiveError } from "../never.ts"
 import { Radix } from "#duncan3142/effect/lib/const"
 
 const unknownToAnyValue = (value: unknown): SdkLogRecord["body"] => {
-	if (
-		typeof value === "string" ||
-		typeof value === "number" ||
-		typeof value === "boolean" ||
-		typeof value === "undefined" ||
-		value === null
-	) {
-		return value
-	} else if (typeof value === "bigint") {
-		return Number(value)
-	} else if (value instanceof Date) {
-		return value.toISOString()
-	} else if (value instanceof Uint8Array) {
-		return value
-	} else if (Array.isArray(value)) {
-		return value.map(unknownToAnyValue)
-	} else if (typeof value === "object") {
-		return Object.fromEntries(
-			Object.entries(value).map(([key, val]) => [key, unknownToAnyValue(val)])
-		)
+	// eslint-disable-next-line @typescript-eslint/switch-exhaustiveness-check -- Allow fallthrough to next switch
+	switch (true) {
+		case value instanceof Date:
+			return value.toISOString()
+		case value instanceof Uint8Array:
+			return value
+		case Array.isArray(value):
+			return value.map(unknownToAnyValue)
+		case value === null:
+			return null
 	}
-	return Inspectable.toStringUnknown(value)
+
+	switch (typeof value) {
+		case "string":
+		case "number":
+		case "boolean":
+		case "undefined":
+			return value
+		case "bigint":
+			return Number(value)
+		case "symbol":
+		case "function":
+			return Inspectable.toStringUnknown(value)
+		case "object":
+			return Object.fromEntries(
+				Object.entries(value).map(([key, val]) => [key, unknownToAnyValue(val)])
+			)
+		default:
+			return ExhaustiveError.throw(value)
+	}
 }
 
 const BaseLogLayer = Effect.gen(function* () {
