79592238566cdb4c39aa289df617071b11aa8bd7
Duncan Giles <duncan3142@gmail.com>
2025-10-12 08:16:40 +0000

    update deps

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,11 +1,22 @@
 export default {
-	extends: ["@commitlint/config-conventional"],
-	rules: {
-		"type-enum": [
-			2,
-			"always",
-			["feat", "fix", "docs", "format", "refactor", "test", "ci", "tools", "deps", "revert"],
-		],
-		"scope-empty": [2, "always"],
-	},
-}
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "format",
+        "refactor",
+        "test",
+        "ci",
+        "tools",
+        "deps",
+        "revert",
+      ],
+    ],
+    "scope-empty": [2, "always"],
+  },
+};
