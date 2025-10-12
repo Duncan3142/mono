3d4cef6de2498e64c0ed0859920c5008bfbb8e9c
Duncan Giles <duncan3142@gmail.com>
2025-10-12 11:15:42 +0000

    console mock

--- pkgs/effect/src/lib/mock/console.mock.ts
+++ pkgs/effect/src/lib/mock/console.mock.ts
@@ -1,51 +1,58 @@
 /* eslint-disable @typescript-eslint/no-empty-function -- Mock */
 import { Console, Effect } from "effect"
+import { type Mocked, vi } from "vitest"
 
 /**
  * Creates a mocked Console instance.
  * @returns A mocked Console instance.
  */
-const make = (): Console.Console => {
+const make = (): Mocked<Console.Console> => {
 	return {
 		[Console.TypeId]: Console.TypeId,
-		log: () => Effect.void,
-		info: () => Effect.void,
-		error: () => Effect.void,
-		debug: () => Effect.void,
-		trace: () => Effect.void,
-		warn: () => Effect.void,
-		assert: () => Effect.void,
-		count: () => Effect.void,
-		countReset: () => Effect.void,
-		group: () => Effect.void,
-		dir: () => Effect.void,
-		table: () => Effect.void,
-		time: () => Effect.void,
-		timeEnd: () => Effect.void,
-		timeLog: () => Effect.void,
-		dirxml: () => Effect.void,
-		groupEnd: Effect.void,
-		clear: Effect.void,
-		unsafe: {
-			assert: () => {},
-			count: () => {},
-			countReset: () => {},
-			debug: () => {},
-			dir: () => {},
-			dirxml: () => {},
-			error: () => {},
-			group: () => {},
-			groupEnd: () => {},
-			info: () => {},
-			log: () => {},
-			table: () => {},
-			time: () => {},
-			timeEnd: () => {},
-			timeLog: () => {},
-			trace: () => {},
-			warn: () => {},
-			clear: () => {},
-			groupCollapsed: () => {},
+		log: vi.fn(() => Effect.void),
+		info: vi.fn(() => Effect.void),
+		error: vi.fn(() => Effect.void),
+		debug: vi.fn(() => Effect.void),
+		trace: vi.fn(() => Effect.void),
+		warn: vi.fn(() => Effect.void),
+		assert: vi.fn((_condition: boolean) => Effect.void),
+		count: vi.fn(() => Effect.void),
+		countReset: vi.fn(() => Effect.void),
+		group: vi.fn(() => Effect.void),
+		dir: vi.fn((_item: unknown) => Effect.void),
+		table: vi.fn((_data: unknown) => Effect.void),
+		time: vi.fn(() => Effect.void),
+		timeEnd: vi.fn(() => Effect.void),
+		timeLog: vi.fn(() => Effect.void),
+		dirxml: vi.fn(() => Effect.void),
+		get groupEnd() {
+			return Effect.void
+		},
+		get clear() {
+			return Effect.void
+		},
+		get unsafe() {
+			return {
+				assert: () => {},
+				count: () => {},
+				countReset: () => {},
+				debug: () => {},
+				dir: () => {},
+				dirxml: () => {},
+				error: () => {},
+				group: () => {},
+				groupEnd: () => {},
+				info: () => {},
+				log: () => {},
+				table: () => {},
+				time: () => {},
+				timeEnd: () => {},
+				timeLog: () => {},
+				trace: () => {},
+				warn: () => {},
+				clear: () => {},
+				groupCollapsed: () => {},
+			}
 		},
 	}
 }
