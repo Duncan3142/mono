58c5deadd13621997f43eb901a325a132cf3e625
Duncan Giles <duncan3142@gmail.com>
2025-10-12 07:48:51 +0000

    remove solid apps

--- apps/solid-squared/ui/.storybook/main.ts
+++
@@ -1,8 +0,0 @@
-import type { StorybookConfig } from "@storybook/sveltekit"
-
-const config: StorybookConfig = {
-	framework: "@storybook/sveltekit",
-	stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|ts|svelte)"],
-}
-
-export default config
