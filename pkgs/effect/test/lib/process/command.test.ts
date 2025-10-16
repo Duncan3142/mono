96b84f1f3654891694f5de25497f299b2ef4f768
Duncan Giles <duncan3142@gmail.com>
2025-10-16 03:05:30 +0000

    command test

--- pkgs/effect/test/lib/process/command.test.ts
+++ pkgs/effect/test/lib/process/command.test.ts
@@ -1,9 +1,11 @@
-import { describe, it, expect } from "@effect/vitest"
+/* eslint-disable @typescript-eslint/unbound-method -- Test*/
+import { describe, it, expect, vitest } from "@effect/vitest"
 import { FileSystem, Path } from "@effect/platform"
 import { NodeContext } from "@effect/platform-node"
-import { Effect, Match, Stream, Console, Data, pipe, Chunk } from "effect"
+import { Effect, Match, Stream, Console, Data, pipe, Chunk, Exit } from "effect"
 import { Command } from "#duncan3142/effect/lib/process"
 import { TagFactory } from "#duncan3142/effect/internal"
+import { MockConsole } from "#duncan3142/effect/lib/mock"
 
 const STAT_ERROR_TAG = TagFactory.make("test-error", `STAT_ERROR`)
 
@@ -14,6 +16,8 @@ class StatError extends Data.TaggedError(STAT_ERROR_TAG)<{
 }> {}
 
 const STAT_ERROR_CODE = 1
+
+const console = MockConsole.make()
 
 const stat = (dir: string, name: string) =>
 	Command.make({
@@ -41,7 +45,7 @@ const stat = (dir: string, name: string) =>
 	}).pipe(Effect.andThen(Stream.runCollect), Effect.map(Chunk.join("\n")))
 
 describe("Command", () => {
-	it.scopedLive("should execute successfully", () =>
+	it.scopedLive("should execute commands", () =>
 		Effect.gen(function* () {
 			const fs = yield* FileSystem.FileSystem
 			const path = yield* Path.Path
@@ -52,9 +56,31 @@ describe("Command", () => {
 				flag: "wx+",
 			})
 
-			const result = yield* stat(tmpDir, fileName)
+			const outRes = yield* stat(tmpDir, fileName)
 
-			expect(result).toBe("test.txt 13")
-		}).pipe(Effect.provide(NodeContext.layer))
+			expect(outRes).toBe("test.txt 13")
+			expect(console.log).toHaveBeenNthCalledWith(1, "test.txt 13")
+			expect(console.error).not.toHaveBeenCalled()
+
+			vitest.clearAllMocks()
+
+			const errRes = yield* stat(tmpDir, "phantom.txt").pipe(Effect.exit)
+
+			expect(errRes).toEqual(
+				Exit.fail(
+					new StatError({
+						exitCode: STAT_ERROR_CODE,
+						command: "stat",
+						args: ["-c", "%n %s", "phantom.txt"],
+					})
+				)
+			)
+			expect(console.log).not.toHaveBeenCalled()
+			expect(console.error).toHaveBeenNthCalledWith(
+				1,
+				"stat: cannot statx 'phantom.txt': No such file or directory"
+			)
+		}).pipe(Effect.provide(NodeContext.layer), Effect.withConsole(console))
 	)
 })
+/* eslint-enable @typescript-eslint/unbound-method -- Test*/
